69f8178e03cac5bb122b43b3429a980dc52c4e44
hasangulbaba <43739652+hasangulbaba@users.noreply.github.com>
2018-10-24 16:51:30 +0300

    Add files via upload

---
+++ database.js
@@ -0,0 +1,32 @@
+class Course{
+
+    constructor(name ,time ,date ,rooms){
+            this.name=name;
+            this.time=time;
+            this.date=date;
+            this.rooms=rooms;
+    }
+
+    toString(){
+
+        return "dersin adı:" +this.name +"dersin zamanı:" + this.time + "ders günü:" +this.date+"dersin sınıfları: "+this.rooms   
+    }
+
+}
+
+class Student {
+
+    constructer(id, name,gpa ,courses){
+        this.id=id;
+        this.name=name;
+        this.gpa=gpa;
+        this.courses;
+        
+    }
+
+    toString() {
+        return "ogrenci id::" +this.id +"ogrenci adi:" + this.name + "ogrenci gpa:" +this.gpa+"ogrenci courses: "+this.courses   
+    }
+}
+
+
