d7f3aeb7d5af3e2ec79b57b49bc375a2f1a6442e
hasangulbaba <43739652+hasangulbaba@users.noreply.github.com>
2018-11-13 21:27:56 +0300

    Update database.js

--- database.js
+++ database.js
@@ -1,32 +1,30 @@
-class courses{
-
-    constructor(name ,time ,date ,rooms){
-            this.name=name;
-            this.time=time;
-            this.date=date;
-            this.rooms=rooms;
+class Course {
+    constructor(name, time, date, rooms) {
+        this.name = name;
+        this.time = time;
+        this.date = date;
+        this.rooms = rooms;    
     }
-
-    toString(){
-
-        return "dersin adı:" +this.name +"dersin zamanı:" + this.time + "ders günü:" +this.date+"dersin sınıfları: "+this.rooms   
-    }
-
-}
-
-class students {
-
-    constructer(id, name,gpa ,courses){
-        this.id=id;
-        this.name=name;
-        this.gpa=gpa;
-        this.courses;
-        
-    }
-
+    
     toString() {
-        return "ogrenci id::" +this.id +"ogrenci adi:" + this.name + "ogrenci gpa:" +this.gpa+"ogrenci courses: "+this.courses   
+        return "name: " + this.name + " time:" + this.time + " date:" + this.date + " rooms:" + this.rooms;
     }
 }
 
-
+class Student {
+    constructor(id, name, gpa, courses) {
+        this.id = id;
+        this.name = name;
+        this.gpa = gpa;
+        this.courses = courses;    
+    }
+    toString() {
+        return "id: " + this.id + " name:" + this.name + " gpa:" + this.gpa + " courses:" + this.courses;
+    }
+}
+class Database {
+	constructor() {
+		this.students = new Map();
+		this.courses = new Map();
+	}
+}
