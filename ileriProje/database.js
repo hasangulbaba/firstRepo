a2c3cfdd6fa899c699557bf736367c9dba201b1d
hasangulbaba <43739652+hasangulbaba@users.noreply.github.com>
2018-12-18 20:49:08 +0300

    Update database.js

--- ileriProje/database.js
+++ ileriProje/database.js
@@ -1,6 +1,12 @@
 class Database {
 	constructor() {
 		this.kitap = new Map();
+		this.macera = new Map();
+		this.bilim = new Map();
+		this.tarih = new Map();
+		this.bilimKurgu = new Map();
+		this.yazilim = new Map();
+		this.polisiye = new Map();
 		this.kitapResim = new Map();
 	}
 }
